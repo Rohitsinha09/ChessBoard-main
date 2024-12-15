0d5616eecbcf77980365a6bd1ae1b6b7bcbd8847
Rohitsinha09 <rohitkumar.rlic@gmail.com>
2024-12-15 16:59:37 +0530

    commit

---
+++ frontend/src/server/index.tsx
@@ -0,0 +1,14 @@
+const backend_url = "https://chessboard-yybr.onrender.com"
+
+ export const Serverapi ={
+    Socket:{
+        url:`${backend_url}`
+    },
+    GameCode:{
+        url:`${backend_url}/Create-Code` 
+    },
+    ValidGameCode:{
+        url:`${backend_url}/Start-ChessGame`
+    }
+ }
+
